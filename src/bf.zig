35e39e4b98c0c6df4054c8c2c003a0671637ccaf
cachecrab <aniseed.truffle.0g@icloud.com>
2024-07-06 23:13:12 +0100

    parse

--- src/bf.zig
+++ src/bf.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const ArenaAllocator = std.heap.ArenaAllocator;
 
 inline fn fallibleAdd(a: anytype, b: anytype) ?@TypeOf(a, b) {
     const res, const overflow = @addWithOverflow(a, b);
@@ -31,6 +32,116 @@ const Token = enum {
     Output,
 };
 
+pub const ParseError = error{
+    UnexpectedClosingBracket,
+    MissingClosingBracket,
+} || Allocator.Error;
+
+pub fn parse(program: []const u8, instAlloc: *ArenaAllocator, tempAlloc: Allocator) ParseError!?*Inst {
+    if (program.len == 0) {
+        return null;
+    }
+
+    const arena = instAlloc.allocator();
+
+    // keep track of who's `jez` we need to set when we encounter an end loop
+    var beforeLoopBeginStack = std.ArrayList(*Inst).init(tempAlloc);
+    defer beforeLoopBeginStack.deinit();
+
+    var cursor: []const u8 = program;
+
+    const firstBlock = consumeBlock(&cursor, null) orelse return null;
+
+    var inst: *Inst = try arena.create(Inst);
+    inst.* = firstBlock.inst;
+
+    const root = inst;
+
+    var block = firstBlock;
+    while (true) {
+        switch (block.next_tok orelse break) {
+            .Incr, .Decr, .Right, .Left, .Input, .Output => {
+                // Chain next block to current
+
+                // Safe .? as this switch prong means at least one fusable instruction in .next_tok
+                const next = consumeBlock(&cursor, block.next_tok).?;
+                defer block = next;
+
+                const nextInst: *Inst = try arena.create(Inst);
+                nextInst.* = next.inst;
+                defer inst = nextInst;
+
+                inst.jez = nextInst;
+                inst.jnz = nextInst;
+            },
+            .BegLoop => {
+                // Don't prepend b.next_tok, we want to skip the [
+                const next = consumeBlock(&cursor, null) orelse return ParseError.MissingClosingBracket;
+                defer block = next;
+
+                const nextInst: *Inst = try arena.create(Inst);
+                nextInst.* = next.inst;
+                defer inst = nextInst;
+
+                try beforeLoopBeginStack.append(inst);
+                inst.jnz = nextInst;
+            },
+            .EndLoop => {
+                const beforeLoopBegin = beforeLoopBeginStack.popOrNull() orelse return ParseError.UnexpectedClosingBracket;
+                inst.jnz = beforeLoopBegin.jnz;
+
+                // Don't prepend b.next_tok, we want to skip the ]
+                const next = consumeBlock(&cursor, null) orelse break;
+                defer block = next;
+
+                const nextInst: *Inst = try arena.create(Inst);
+                nextInst.* = next.inst;
+                defer inst = nextInst;
+
+                inst.jez = nextInst;
+                beforeLoopBegin.jez = nextInst;
+            },
+        }
+    }
+
+    if (beforeLoopBeginStack.items.len != 0) {
+        return ParseError.MissingClosingBracket;
+    }
+
+    return root;
+}
+
+test "parse" {
+    const testing = std.testing;
+
+    const expect = testing.expectEqual;
+    const assert = testing.expect;
+
+    var instAlloc = ArenaAllocator.init(testing.allocator);
+    defer instAlloc.deinit();
+
+    const tempAlloc = testing.allocator;
+
+    const res = try parse("++[->++<]", &instAlloc, tempAlloc);
+    const inst = res.?;
+
+    try expect(2, inst.add);
+    try expect(null, inst.jez);
+    try assert(null != inst.jnz);
+
+    const inner = inst.jnz.?;
+    try expect(-1, inner.add);
+    try expect(1, inner.shift);
+    try assert(null != inner.jnz);
+    try assert(inner.jnz.? == inner.jez.?);
+
+    const inner2 = inner.jnz.?;
+    try expect(2, inner2.add);
+    try expect(-1, inner2.shift);
+    try expect(inner, inner2.jnz.?);
+    try expect(null, inner2.jez);
+}
+
 /// A Block is a partial instruction parsing result, namely as many
 /// tokens have been fused into the Inst as possible,
 /// excluding loop constructs (jez and jnz both null).
@@ -57,7 +168,7 @@ fn consumeBlock(cursor: *[]const u8, prepend: ?Token) ?Block {
 
     const ParseState = enum { Add, Shift, Input, Output };
 
-    var parseState = ParseState.Add;
+    var state = ParseState.Add;
 
     var inst = Inst{};
     var token: ?Token = null;
@@ -67,41 +178,41 @@ fn consumeBlock(cursor: *[]const u8, prepend: ?Token) ?Block {
         prepend_once = null;
 
         switch (token orelse break) {
-            .Incr => switch (parseState) {
+            .Incr => switch (state) {
                 .Add => inst.add = fallibleAdd(inst.add, 1) orelse break,
                 else => break,
             },
-            .Decr => switch (parseState) {
+            .Decr => switch (state) {
                 .Add => inst.add = fallibleSub(inst.add, 1) orelse break,
                 else => break,
             },
-            .Right => switch (parseState) {
+            .Right => switch (state) {
                 .Add => {
-                    parseState = ParseState.Shift;
+                    state = ParseState.Shift;
                     inst.shift = fallibleAdd(inst.shift, 1) orelse break;
                 },
                 .Shift => inst.shift = fallibleAdd(inst.shift, 1) orelse break,
                 else => break,
             },
-            .Left => switch (parseState) {
+            .Left => switch (state) {
                 .Add => {
-                    parseState = ParseState.Shift;
+                    state = ParseState.Shift;
                     inst.shift = fallibleSub(inst.shift, 1) orelse break;
                 },
                 .Shift => inst.shift = fallibleSub(inst.shift, 1) orelse break,
                 else => break,
             },
-            .Input => switch (parseState) {
+            .Input => switch (state) {
                 .Add, .Shift => {
-                    parseState = ParseState.Input;
+                    state = ParseState.Input;
                     inst.input = fallibleAdd(inst.input, 1) orelse break;
                 },
                 .Input => inst.input = fallibleAdd(inst.input, 1) orelse break,
                 else => return Block{ .inst = inst, .next_tok = token },
             },
-            .Output => switch (parseState) {
+            .Output => switch (state) {
                 .Add, .Shift, .Input => {
-                    parseState = ParseState.Output;
+                    state = ParseState.Output;
                     inst.output = fallibleAdd(inst.output, 1) orelse break;
                 },
                 .Output => inst.output = fallibleAdd(inst.output, 1) orelse break,
